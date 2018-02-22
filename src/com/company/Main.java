a2758fc43a213241f20162ba23ccf2d2e205b009
Joan Ytterborg <j.ytterborg@gmail.com>
2018-02-22 09:45:57 +0100

    Created lanterna and started in main.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,33 @@
 package com.company;
 
+
+import com.googlecode.lanterna.*;
+import com.googlecode.lanterna.input.Key;
+import com.googlecode.lanterna.terminal.Terminal;
+
+import java.nio.charset.Charset;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Terminal terminal = TerminalFacade.createTerminal(System.in, System.out, Charset.forName("UTF8"));
+        terminal.enterPrivateMode();
+        
+
+        int score = 0;
+
+        Key key;
+
+        Board.generateBoard();
+
+        while (true) {
+
+
+            Move.moveHero(key);
+            Win.checkForWin();
+
+
+            score++;
+        }
     }
 }
